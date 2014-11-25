7cf3555e374187565f6c53a964a02365468360bc
Michael <michael@michael-VirtualBox.(none)>
2014-11-25 11:20:36 +0000

    Changed title of homepage

--- sites/all/themes/systemick/template.php
+++ sites/all/themes/systemick/template.php
@@ -60,15 +60,11 @@ function systemick_preprocess_maintenance_page(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("html" in this case.)
  */
-/* -- Delete this line if you want to use this function
 function systemick_preprocess_html(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // The body tag's classes are controlled by the $classes_array variable. To
-  // remove a class from $classes_array, use array_diff().
-  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
+  if (drupal_is_front_page()) {
+    $variables['head_title'] = 'Open source development in Leeds West Yorkshire using Drupal CMS, Node JS and Angular JS | Systemick';
+  }
 }
-// */
 
 /**
  * Override or insert variables into the page templates.
